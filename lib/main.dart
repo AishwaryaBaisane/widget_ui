858d18cb2449f26be20a82b90727ca1373c5b5f2
admin <baisanehira@gmail.com>
2024-04-15 03:49:55 -0700

    5.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,7 @@
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: SafeArea(
-//         child: Scaffold(
-//         appBar :AppBar(
-//               child: Text(
-//                 'Emoji',
-//                 style: TextStyle(color: Colors.white, fontSize: 30),
-//               ),
-//             ),
-//           ),
-//           body: Align(
-//             child: Center(
-//               child: Container(
-//                 height: 330,
-//                 width: 330,
-//                 alignment: Alignment.center,
-//                 decoration: BoxDecoration(
-//                   color: Color(0xffFF9800),
-//                   shape: BoxShape.circle,
-//                 ),
-//                 child: Container(
-//                   height: 260,
-//                   width: 260,
-//                   alignment: Alignment(-0.65,-0.50),
-//                   decoration: BoxDecoration(
-//                       color: Colors.orange,
-//                       shape: BoxShape.circle,
-//                       border: Border(bottom: BorderSide(color: Colors.white,width: 15))
-//                   ),
-//                   child: Container(
-//                     height: 78,
-//                     width: 78,
-//                     alignment: Alignment(75,0),
-//                     decoration: BoxDecoration(
-//                       color: Colors.white,
-//                       shape: BoxShape.circle,
-//                     ),
-//                     child: Container(
-//                       height: 76,
-//                       width: 75,
-//                       decoration: BoxDecoration(
-//                         color: Colors.white,
-//                         shape: BoxShape.circle,
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+
 import 'package:flutter/material.dart';
+
+import 'home/components/5.1/Emoji.dart';
 
 //
 void main() {
@@ -80,73 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Color(0xffff9700),
-          title: Text(
-            'Emoji',
-            style: TextStyle(
-              color: Colors.white,fontSize: 30
-            ),
-          ),
-        ),
-        body: Align(
-          child: Center(
-            child: Container(
-              height: 330,
-              width: 330,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Color(0xffFF9800),
-                shape: BoxShape.circle,
-              ),
-              child: Container(
-                height: 260,
-                width: 260,
-                alignment: Alignment(-0.65, -0.50),
-                decoration: BoxDecoration(
-                    color: Colors.orange,
-                    shape: BoxShape.circle,
-                    border: Border(
-                        bottom: BorderSide(color: Colors.white, width: 15))),
-                child: Container(
-                  height: 78,
-                  width: 78,
-                  alignment: Alignment(75, 0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Container(
-                    height: 76,
-                    width: 75,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
